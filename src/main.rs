a9ea805069ec3be8b17f8b69eada02adfe085ffd
EmperorParzival <29981946+EmperorParzival@users.noreply.github.com>
2021-05-03 10:18:33 -0400

    fix: favicon.ico route preventing duplicate requests

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,14 @@ use utility::{multiple_roblox, request};
 use warp::Filter;
 
 async fn launch_roblox(token: String) -> Result<impl warp::Reply, warp::Rejection> {
+	println!("Getting authentication ticket...");
+
 	Ok(match request::launch_roblox(token).await {
-		Ok(_) => "Success! You may now close this tab.",
+		Ok(_) => {
+			println!("Roblox has launched successfully!");
+
+			"Success! You may now close this tab."
+		},
 		Err(_) =>
 			"Something went wrong when trying to open Roblox. Please see the console for more \
 			 information.",
@@ -16,13 +22,15 @@ async fn launch_roblox(token: String) -> Result<impl warp::Reply, warp::Rejectio
 #[tokio::main]
 #[allow(unused_must_use)]
 async fn main() -> Result<(), warp::Rejection> {
-	multiple_roblox::setup();
+	multiple_roblox::start();
 
+	let favicon = warp::path!("favicon.ico").map(|| "");
 	let default = warp::get().and(warp::path::param()).and_then(launch_roblox);
 	let handler =
 		warp::any().map(|| "You're in the wrong place; please use the URL provided by the bot.");
 
-	warp::serve(default.or(handler))
+	println!("Apathy Client listening on 127.0.0.1:3000");
+	warp::serve(favicon.or(default).or(handler))
 		.run(([127, 0, 0, 1], 3000))
 		.await;
 
